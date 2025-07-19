fc54674a16f395e94a572dae20133e9131536607
AndyCiu <pc01pc013@gmail.com>
2025-07-19 20:37:41 +0800

    Add QR Code Generate

--- CryptographyService_JS.cs
+++ CryptographyService_JS.cs
@@ -1,35 +1,29 @@
 ﻿using Microsoft.JSInterop;
 
 namespace sealed_registered_ballot_blazor;
-public class CryptographyService_JS : ICryptographyService
+public class CryptographyService_JS(IJSRuntime jsInterop) : ICryptographyService
 {
-    private readonly IJSRuntime _jsInterop;
-
-    public CryptographyService_JS(IJSRuntime jsInterop)
-    {
-        _jsInterop = jsInterop;
-    }
     class InvokeReturnData_generateKey
     {
-        public string PublicKey { get; set; }
-        public string PrivateKey { get; set; }
+        public string PublicKey { get; set; } = "";
+        public string PrivateKey { get; set; } = "";
     }
     public async Task<(string rsaPublicstr, string rsaPrivatestr)> GetRSAKeyPair()
     {
-        var result = await _jsInterop.InvokeAsync<InvokeReturnData_generateKey>("SRBB_generateKey_RSA");
+        var result = await jsInterop.InvokeAsync<InvokeReturnData_generateKey>("SRBB_generateKey_RSA");
         return (result.PublicKey, result.PrivateKey);
     }
 
     public async Task<string> GetAESKey()
     {
-        return await _jsInterop.InvokeAsync<string>("SRBB_generateKey_AES");
+        return await jsInterop.InvokeAsync<string>("SRBB_generateKey_AES");
     }
 
     public Dictionary<string, int> CreateVoterOptionDict(int optCount)
     {
         const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        Random random = new Random();
-        Dictionary<string, int> result = new Dictionary<string, int>();
+        Random random = new();
+        Dictionary<string, int> result = [];
         int totalnum = optCount > 26 ? 26 : optCount;
         bool[] optvaluesflag = new bool[totalnum];
 
@@ -54,9 +48,9 @@ public class CryptographyService_JS : ICryptographyService
 
     public Dictionary<string, int> CreateVoterOptionDict(string votestr)
     {
-        Dictionary<string, int> result = new Dictionary<string, int>();
+        Dictionary<string, int> result = [];
         string[] tmpOptArr = votestr.Split('=');
-        foreach(string key in tmpOptArr)
+        foreach (string key in tmpOptArr)
         {
             string[] tmpvalues = key.Split(':');
             result.Add(tmpvalues[0], Convert.ToInt32(tmpvalues[1]));
@@ -75,25 +69,25 @@ public class CryptographyService_JS : ICryptographyService
 
             result = result + key + ":" + val + "=";
         }
-        return result.Substring(0, result.Length - 1);
+        return result[..^1];
     }
 
     public async Task<string> EncryptVote(string votestr, string ballotPublickey, string voterkey)
     {
-        var result1 = await _jsInterop.InvokeAsync<byte[]>("SRBB_encrypt_RSA", ballotPublickey, votestr);
+        var result1 = await jsInterop.InvokeAsync<byte[]>("SRBB_encrypt_RSA", ballotPublickey, votestr);
         var result1str = Convert.ToBase64String(result1);
 
-        var result2 = await _jsInterop.InvokeAsync<string>("SRBB_encrypt_AES", voterkey, result1str);
+        var result2 = await jsInterop.InvokeAsync<string>("SRBB_encrypt_AES", voterkey, result1str);
 
         return result2;
     }
 
     public async Task<string> DecryptVote(string voteEncryptStr, string ballotPrivatekey, string voterkey)
     {
-        var result1 = await _jsInterop.InvokeAsync<string>("SRBB_decrypt_AES", voterkey, voteEncryptStr);
+        var result1 = await jsInterop.InvokeAsync<string>("SRBB_decrypt_AES", voterkey, voteEncryptStr);
 
         var tmpstr2 = Convert.FromBase64String(result1);
-        var result2 = await _jsInterop.InvokeAsync<string>("SRBB_decrypt_RSA", ballotPrivatekey, tmpstr2);
+        var result2 = await jsInterop.InvokeAsync<string>("SRBB_decrypt_RSA", ballotPrivatekey, tmpstr2);
 
         return result2;
     }
